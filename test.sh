f25c9ad61ed7ea782f6c27ca51b3ece918207fc0
Joshua Enokson <kilograhm@pm.me>
2021-07-24 12:16:03 +0500

    added test coverage script

--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
-cargo test -- --nocapture
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+export RUSTDOCFLAGS="-Cpanic=abort"
+cargo +nightly build
+cargo +nightly test --lib
+grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
+
