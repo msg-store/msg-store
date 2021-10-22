3a5575d97c564bd30095468709054b9bcf5f4aef
joshua enokson <kilograhm@pm.me>
2021-10-23 00:33:07 +0500

    added Keeper trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
-#![allow(dead_code)]
 
+pub mod database;
 pub mod store;
+pub mod uuid;
+
+use crate::{
+    uuid::Uuid,
+    store::{ Package, PacketMetaData }
+};
+
+pub trait Keeper {
+    fn add(&mut self, package: &Package);
+    fn get(&mut self, uuid: &Uuid) -> Option<String>;
+    fn del(&mut self, uuid: &Uuid);
+    fn fetch(&mut self) -> Vec<PacketMetaData>;
+}
