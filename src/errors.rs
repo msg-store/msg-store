735c400cea42817e5f7f4b6534bc6c9b9a6c0d3d
joshua enokson <kilograhm@pm.me>
2021-11-19 23:50:35 +0500

    impl Display

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,12 @@
+use std::fmt::Display;
 
 #[derive(Debug)]
 pub struct DbError(pub String);
+impl Display for DbError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 #[derive(Debug)]
 pub enum Error {
@@ -9,4 +15,13 @@ pub enum Error {
     LacksPriority,
     DbError(DbError),
     SyncError
+}
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::DbError(db_error) => write!(f, "{}", db_error),
+            _ => write!(f, "{}", self)
+        }
+        
+    }
 }
