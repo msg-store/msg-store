3e08a0cb92207d56904a13e59735d910b795218d
joshua enokson <kilograhm@pm.me>
2022-02-13 09:27:30 -0800

    refactored code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,2 @@
-
 pub mod core;
 pub mod database;
-
-// pub mod errors;
-// pub mod mem;
-// pub mod store;
-// pub mod uuid;
-
-// pub use crate::{
-//     store::Store,
-//     mem::MemDb,
-//     uuid::Uuid
-// };
