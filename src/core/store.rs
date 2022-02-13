018d899aa92323e814e901c6983d0dbf397d8098
joshua enokson <kilograhm@pm.me>
2022-02-13 09:06:12 -0800

    add db-plugin mod

--- src/core/store.rs
+++ src/core/store.rs
@@ -1,4 +1,4 @@
-use crate::{
+use crate::core::{
     errors::Error,
     uuid::{
         UuidManager,
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 
-pub mod errors;
-pub mod mem;
-pub mod store;
-pub mod uuid;
+pub mod core;
+pub mod database;
 
-pub use crate::{
-    store::Store,
-    mem::MemDb,
-    uuid::Uuid
-};
+// pub mod errors;
+// pub mod mem;
+// pub mod store;
+// pub mod uuid;
+
+// pub use crate::{
+//     store::Store,
+//     mem::MemDb,
+//     uuid::Uuid
+// };
