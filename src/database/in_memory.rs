9501043cd7b69b3402b0472ec5961d88e1b42efe
joshua enokson <kilograhm@pm.me>
2022-02-17 12:10:01 -0800

    added database error types to memdb

--- src/database/in_memory.rs
+++ src/database/in_memory.rs
@@ -1,8 +1,27 @@
 use bytes::Bytes;
 use crate::core::uuid::Uuid;
-pub use crate::database::Db;
+pub use crate::database::{Db, DatabaseError, DatabaseErrorTy};
 use std::collections::BTreeMap;
 use std::sync::Arc;
+
+macro_rules! memdb_error {
+    ($err_ty:expr) => {
+        DatabaseError {
+            err_ty: $err_ty,
+            file: file!(),
+            line: line!(),
+            msg: None
+        }
+    };
+    // ($err_ty:expr, $msg:expr) => {
+    //     DatabaseError {
+    //         err_ty: $err_ty,
+    //         file: file!(),
+    //         line: line!(),
+    //         msg: Some($msg.to_string())
+    //     }
+    // };
+}
 
 pub struct MemDb {
     msgs: BTreeMap<Arc<Uuid>, Bytes>,
@@ -17,23 +36,23 @@ impl MemDb {
     }
 }
 impl Db for MemDb {
-    fn add(&mut self, uuid: Arc<Uuid>, msg: Bytes, msg_byte_size: u32) -> Result<(), String> {
+    fn add(&mut self, uuid: Arc<Uuid>, msg: Bytes, msg_byte_size: u32) -> Result<(), DatabaseError> {
         self.msgs.insert(uuid.clone(), msg);
         self.byte_size_data.insert(uuid, msg_byte_size);
         Ok(())
     }
-    fn get(&mut self, uuid: Arc<Uuid>) -> Result<Bytes, String> {
+    fn get(&mut self, uuid: Arc<Uuid>) -> Result<Bytes, DatabaseError> {
         match self.msgs.get(&uuid) {
             Some(msg) => Ok(msg.clone()),
-            None => Err("msg not found".to_string())
+            None => Err(memdb_error!(DatabaseErrorTy::MsgNotFound))
         }
     }
-    fn del(&mut self, uuid: Arc<Uuid>) -> Result<(), String> {
+    fn del(&mut self, uuid: Arc<Uuid>) -> Result<(), DatabaseError> {
         self.msgs.remove(&uuid);
         self.byte_size_data.remove(&uuid);
         Ok(())
     }
-    fn fetch(&mut self) -> Result<Vec<(Arc<Uuid>, u32)>, String> {
+    fn fetch(&mut self) -> Result<Vec<(Arc<Uuid>, u32)>, DatabaseError> {
         Ok(vec![])
     }
 }
