1f7c0b76176426d41687143b262a097ca144bed5
joshua enokson <kilograhm@pm.me>
2022-02-17 21:05:53 -0800

    added error types to add msg api

--- src/api/msg/add.rs
+++ src/api/msg/add.rs
@@ -1,25 +1,88 @@
 use crate::api::{Database, lock};
-use crate::api::error_codes::{ self, log_err };
 use crate::api::file_storage::{
     rm_from_file_storage,
     add_to_file_storage,
     FileStorage
 };
 use crate::api::stats::Stats;
+use crate::api::file_storage::FileStorageError;
 use crate::core::store::{Store, StoreErrorTy};
-// use crate::core::store::Error;
+use crate::database::DatabaseError;
 use crate::core::uuid::Uuid;
 use bytes::{Bytes, BytesMut};
 use futures::{Stream, StreamExt};
-use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
+use std::fmt::Display;
 use std::marker::Unpin;
 use std::sync::{Arc,Mutex};
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Body {
-    priority: u32,
-    msg: String,
+#[derive(Debug)]
+pub enum MsgError {
+    FileStorageNotConfigured,
+    InvalidBytesizeOverride,
+    InvalidPriority,
+    MissingBytesizeOverride,
+    MissingHeaders,
+    MissingPriority,
+    MalformedHeaders,
+    MsgExceedesStoreMax,
+    MsgExceedesGroupMax,
+    MsgLacksPriority
+}
+
+#[derive(Debug)]
+pub enum AddErrorTy {
+    CouldNotFindFileStorage,
+    CouldNotGetNextChunkFromPayload,
+    CouldNotParseChunk,
+    DatabaseError(DatabaseError),
+    FileStorageError(FileStorageError),
+    LockingError,
+    MsgError(MsgError),
+    StoreError(StoreErrorTy)
+}
+impl Display for AddErrorTy {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+#[derive(Debug)]
+pub struct AddError {
+    pub err_ty: AddErrorTy,
+    pub file: &'static str,
+    pub line: u32,
+    pub msg: Option<String>
+}
+
+impl Display for AddError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "ADD_MSG_ERROR: {}. file: {}, line: {}.", self.err_ty, self.file, self.line)?;
+        if let Some(msg) = &self.msg {
+            write!(f, "{}", msg)
+        } else {
+            Ok(())
+        }
+    }   
+}
+
+macro_rules! add_msg_error {
+    ($err_ty:expr) => {
+        AddError {
+            err_ty: $err_ty,
+            file: file!(),
+            line: line!(),
+            msg: None
+        }
+    };
+    ($err_ty:expr, $msg:expr) => {
+        AddError {
+            err_ty: $err_ty,
+            file: file!(),
+            line: line!(),
+            msg: Some($msg.to_string())
+        }
+    };
 }
 
 pub trait Chunky: Stream<Item=Result<Bytes, &'static str>> + Unpin { }
@@ -29,7 +92,7 @@ pub async fn handle<T: Chunky>(
     file_storage: &Option<Mutex<FileStorage>>,
     stats: &Mutex<Stats>,
     database: &Mutex<Database>,
-    payload: &mut T) -> Result<Arc<Uuid>, &'static str> {
+    payload: &mut T) -> Result<Arc<Uuid>, AddError> {
 
         let mut metadata_string = String::new();
         let mut msg_chunk = BytesMut::new();
@@ -39,17 +102,11 @@ pub async fn handle<T: Chunky>(
         while let Some(chunk) = payload.next().await {
             let chunk = match chunk {
                 Ok(chunk) => Ok(chunk),
-                Err(error) => {
-                    log_err(error, file!(), line!(), "");
-                    Err(error)
-                }
+                Err(error) => Err(add_msg_error!(AddErrorTy::CouldNotGetNextChunkFromPayload, error))
             }?;
             let mut chunk_string = match String::from_utf8(chunk.to_vec()) {
                 Ok(chunk_string) => Ok(chunk_string),
-                Err(error) => {
-                    log_err(error_codes::COULD_NOT_PARSE_CHUNK, file!(), line!(), error.to_string());
-                    Err(error_codes::COULD_NOT_PARSE_CHUNK)
-                }
+                Err(error) => Err(add_msg_error!(AddErrorTy::CouldNotParseChunk, error))
             }?;
             chunk_string = chunk_string.trim_start().to_string();
             // debug!("recieved chunk: {}", chunk_string);
@@ -57,7 +114,7 @@ pub async fn handle<T: Chunky>(
             if metadata_string.contains("?") {
                 // debug!("msg start found!");
                 if metadata_string.len() == 0 {
-                    return Err(error_codes::MISSING_HEADERS);
+                    return Err(add_msg_error!(AddErrorTy::MsgError(MsgError::MissingHeaders)));
                 }
                 match metadata_string.split_once("?") {
                     Some((metadata_section, msg_section)) => {
@@ -65,19 +122,18 @@ pub async fn handle<T: Chunky>(
                             let kv = pair.trim_end().trim_start().split("=").map(|txt| txt.to_string()).collect::<Vec<String>>();
                             let k = match kv.get(0) {
                                 Some(k) => Ok(k.clone()),
-                                None => Err(error_codes::MALFORMED_HEADERS)
+                                None => Err(add_msg_error!(AddErrorTy::MsgError(MsgError::MalformedHeaders)))
                             }?;
                             let v = match kv.get(1) {
                                 Some(v) => Ok(v.clone()),
-                                None => Err(error_codes::MALFORMED_HEADERS)
+                                None => Err(add_msg_error!(AddErrorTy::MsgError(MsgError::MalformedHeaders)))
                             }?;
                             metadata.insert(k, v);
                         };
                         msg_chunk.extend_from_slice(msg_section.as_bytes());
                     },
                     None => {
-                        log_err(error_codes::COULD_NOT_PARSE_CHUNK, file!(), line!(), "");
-                        return Err(error_codes::COULD_NOT_PARSE_CHUNK)
+                        return Err(add_msg_error!(AddErrorTy::CouldNotParseChunk))
                     }
                 }
                 if let Some(save_to_file_value) = metadata.remove("saveToFile") {
@@ -86,7 +142,7 @@ pub async fn handle<T: Chunky>(
                             while let Some(_chunk) = payload.next().await {
     
                             }
-                            return Err(error_codes::FILE_STORAGE_NOT_CONFIGURED);
+                            return Err(add_msg_error!(AddErrorTy::MsgError(MsgError::FileStorageNotConfigured)));
                         }
                         save_to_file = true;
                     }
@@ -96,11 +152,11 @@ pub async fn handle<T: Chunky>(
         }
     
         let priority: u32 = match metadata.remove("priority") {
-            Some(priority) => match priority.parse() {
+            Some(priority) => match priority.parse::<u32>() {
                 Ok(priority) => Ok(priority),
-                Err(_error) => Err(error_codes::INVALID_PRIORITY)
+                Err(error) => Err(add_msg_error!(AddErrorTy::MsgError(MsgError::InvalidPriority), error))
             },
-            None => Err(error_codes::MISSING_PRIORITY)
+            None => Err(add_msg_error!(AddErrorTy::MsgError(MsgError::MissingPriority)))
         }?;
 
         let (msg_byte_size, msg) = {
@@ -108,7 +164,7 @@ pub async fn handle<T: Chunky>(
                 if let Some(byte_size_override_str) = metadata.get("byteSizeOverride") {
                     let msg_byte_size = match byte_size_override_str.parse::<u64>() {
                         Ok(byte_size_override) => Ok(byte_size_override),
-                        Err(_error) => Err(error_codes::INVALID_BYTESIZE_OVERRIDE)
+                        Err(error) => Err(add_msg_error!(AddErrorTy::MsgError(MsgError::InvalidBytesizeOverride), error))
                     }?;
                     let msg_parse = metadata
                         .iter()
@@ -117,40 +173,34 @@ pub async fn handle<T: Chunky>(
                         .join("&");
                     Ok((msg_byte_size, msg_parse))
                 } else {
-                    Err(error_codes::MISSING_BYTESIZE_OVERRIDE)
+                    Err(add_msg_error!(AddErrorTy::MsgError(MsgError::MissingBytesizeOverride)))
                 }
             } else {
                 while let Some(chunk) = payload.next().await {
                     let chunk = match chunk {
                         Ok(chunk) => Ok(chunk),
-                        Err(error) => {
-                            log_err(error, file!(), line!(), "");
-                            Err(error)
-                        }
+                        Err(error) => Err(add_msg_error!(AddErrorTy::CouldNotGetNextChunkFromPayload, error))
                     }?;
                     msg_chunk.extend_from_slice(&chunk);
                 }
                 match String::from_utf8(msg_chunk.to_vec()) {
                     Ok(msg) => Ok((msg.len() as u64, msg)),
-                    Err(error) => {
-                        log_err(error_codes::COULD_NOT_PARSE_CHUNK, file!(), line!(), error.to_string());
-                        return Err(error_codes::COULD_NOT_PARSE_CHUNK)
-                    }
+                    Err(error) => Err(add_msg_error!(AddErrorTy::CouldNotParseChunk, error))
                 }
             }
         }?;
         let add_result = {
-            let mut store = lock(&store)?;
+            let mut store = match lock(&store) {
+                Ok(store) => Ok(store),
+                Err(error) => Err(add_msg_error!(AddErrorTy::LockingError, error))
+            }?;
             match store.add(priority, msg_byte_size) {
                 Ok(add_result) => Ok(add_result),
                 Err(error) => match error.err_ty {
-                    StoreErrorTy::ExceedesStoreMax => Err(error_codes::MSG_EXCEEDES_STORE_MAX),
-                    StoreErrorTy::ExceedesGroupMax => Err(error_codes::MSG_EXCEEDES_GROUP_MAX),
-                    StoreErrorTy::LacksPriority => Err(error_codes::MSG_LACKS_PRIORITY),
-                    error => {
-                        log_err(error_codes::STORE_ERROR, file!(), line!(), error.to_string());
-                        Err(error_codes::STORE_ERROR)
-                    }
+                    StoreErrorTy::ExceedesStoreMax => Err(add_msg_error!(AddErrorTy::MsgError(MsgError::MsgExceedesStoreMax))),
+                    StoreErrorTy::ExceedesGroupMax => Err(add_msg_error!(AddErrorTy::MsgError(MsgError::MsgExceedesGroupMax))),
+                    StoreErrorTy::LacksPriority => Err(add_msg_error!(AddErrorTy::MsgError(MsgError::MsgLacksPriority))),
+                    error_ty => Err(add_msg_error!(AddErrorTy::StoreError(error_ty)))
                 },
             }
         }?;
@@ -159,44 +209,54 @@ pub async fn handle<T: Chunky>(
         let mut deleted_count = 0;
         for uuid in add_result.msgs_removed.into_iter() {
             {
-                let mut database = lock(&database)?;
+                let mut database = match lock(&database) {
+                    Ok(database) => Ok(database),
+                    Err(error) => Err(add_msg_error!(AddErrorTy::LockingError, error))
+                }?;
                 if let Err(error) = database.del(uuid.clone()) {
-                    log_err(error_codes::DATABASE_ERROR, file!(), line!(), error.to_string());
-                    return Err(error_codes::DATABASE_ERROR)
+                    return Err(add_msg_error!(AddErrorTy::DatabaseError(error)))
                 }
             }
             if let Some(file_storage) = &file_storage {
-                let mut file_storage = lock(&file_storage)?;
+                let mut file_storage = match lock(&file_storage) {
+                    Ok(file_storage) => Ok(file_storage),
+                    Err(error) => Err(add_msg_error!(AddErrorTy::LockingError, error))
+                }?;
                 if let Err(error) = rm_from_file_storage(&mut file_storage, &uuid) {
-                    // let tmp_err = error.to_string();
-                    // log_err(&tmp_err, file!(), line!(), "Could not removed file from file list.");
-                    return Err("FS ERROR");
+                    return Err(add_msg_error!(AddErrorTy::FileStorageError(error)));
                 }
             }
             deleted_count += 1;
         }
         {
-            let mut stats = lock(&stats)?;
+            let mut stats = match lock(&stats) {
+                Ok(stats) => Ok(stats),
+                Err(error) => Err(add_msg_error!(AddErrorTy::LockingError, error))
+            }?;
             stats.deleted += deleted_count;
             stats.inserted += 1;
         }
         // add to file manager if needed
         if save_to_file {
             if let Some(file_storage) = file_storage {
-                let mut file_storage = lock(&file_storage)?;
+                let mut file_storage = match lock(&file_storage) {
+                    Ok(file_storage) => Ok(file_storage),
+                    Err(error) => Err(add_msg_error!(AddErrorTy::LockingError, error))
+                }?;
                 if let Err(error) = add_to_file_storage(&mut file_storage, add_result.uuid.clone(), &msg_chunk, payload).await {
-                    return Err("FS ERROR")
+                    return Err(add_msg_error!(AddErrorTy::FileStorageError(error)))
                 }
             } else {
-                log_err(error_codes::COULD_NOT_FIND_FILE_STORAGE, file!(), line!(), "");
-                return Err(error_codes::COULD_NOT_FIND_FILE_STORAGE);
+                return Err(add_msg_error!(AddErrorTy::CouldNotFindFileStorage));
             }
         }
         {        
-            let mut database = lock(&database)?;
+            let mut database = match lock(&database) {
+                Ok(database) => Ok(database),
+                Err(error) => Err(add_msg_error!(AddErrorTy::LockingError, error))
+            }?;
             if let Err(error) = database.add(add_result.uuid.clone(), Bytes::copy_from_slice(msg.as_bytes()), msg_byte_size) {
-                log_err(error_codes::DATABASE_ERROR, file!(), line!(), error.to_string());
-                return Err(error_codes::DATABASE_ERROR);
+                return Err(add_msg_error!(AddErrorTy::DatabaseError(error)));
             }
         }
         Ok(add_result.uuid)
