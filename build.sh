8b6f255102228dc0891bb135acb8020dc2e59420
joshua enokson <kilograhm@pm.me>
2022-02-24 19:23:14 -0800

    added docker build files

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-docker build -t msg-store-armv7-builder .
+# docker build -t msg-store-armv7-builder-2 .
+docker build -t msg-store-http-server builds/http-server
+docker build -t msg-store-http-server:armv7 builds/http-server/armv7
