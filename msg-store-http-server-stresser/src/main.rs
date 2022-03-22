e1aa12fb31807ee884ffdc1148a4e7e0a018ff6b
joshua enokson <kilograhm@pm.me>
2022-03-22 13:46:03 -0700

    changed strings into path types

--- msg-store-http-server-stresser/src/main.rs
+++ msg-store-http-server-stresser/src/main.rs
@@ -8,6 +8,7 @@ use futures::future::*;
 use std::fs::{File, create_dir_all, remove_dir_all};
 use std::io::{BufReader, BufWriter, Write, Read};
 use std::ops::Deref;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Output};
 use std::rc::Rc;
 use std::sync::{Arc, Mutex};
@@ -17,7 +18,7 @@ use rand::{Rng};
 use rayon::prelude::*;
 use tempdir::TempDir;
 
-fn get_file_name() -> String {
+fn get_file_name() -> PathBuf {
     let characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890".as_bytes().to_vec();
     let mut rng = rand::thread_rng();
     let mut name_parts: Vec<u8> = Vec::with_capacity(30);
@@ -26,7 +27,7 @@ fn get_file_name() -> String {
         name_parts.push(new_char);
     }
     let name = String::from_utf8(name_parts).unwrap();
-    name
+    PathBuf::from(name)    
 }
 
 fn generate_contents(iterations: u32, writer: &mut BufWriter<File>) {
@@ -41,16 +42,21 @@ fn generate_contents(iterations: u32, writer: &mut BufWriter<File>) {
     }
 }
 
-fn create_file(source_path: &str) -> String {
+fn create_file(source_path: &Path) -> PathBuf {
     let file_name = get_file_name();
-    let file_path = format!("{}/{}", source_path, file_name);
+    let file_path = {
+        let mut file_path = source_path.to_path_buf();
+        file_path.push(&file_name);
+        file_path
+    };
+    // let file_path = format!("{}/{}", source_path, file_name);
     let file = File::create(file_path).unwrap();
     let mut buf = BufWriter::new(file);
     generate_contents(10000, &mut buf);
     file_name
 }
 
-fn create_files(source_path: &str, count: u32) -> Vec<String> {
+fn create_files(source_path: &Path, count: u32) -> Vec<PathBuf> {
     let next = Arc::new(Mutex::new(0));
     (1..=count).into_par_iter().map(|_| {
         {
@@ -61,24 +67,29 @@ fn create_files(source_path: &str, count: u32) -> Vec<String> {
             *counter += 1;
         }
         create_file(&source_path)
-    }).collect::<Vec<String>>()
+    }).collect::<Vec<PathBuf>>()
 }
 
-async fn send_file(source_dir: &str, file_name: String) {
-    let mut file = File::open(format!("{}/{}", source_dir, file_name)).unwrap();
+async fn send_file(source_dir: &Path, file_name: PathBuf) {
+    let file_path = {
+        let mut file_path = source_dir.to_path_buf();
+        file_path.push(&file_name);
+        file_path
+    };
+    let mut file = File::open(file_path).unwrap();
     let mut buf = String::new();
     file.read_to_string(&mut buf).unwrap();
     let client = Client::new();
     let req = Request::builder()
         .method(Method::POST)
         .uri("http://127.0.0.1:8080/api/msg")
-        .body(Body::from(format!("priority=1&saveToFile=true&bytesizeOverride=1&fileName={}?{}", file_name, buf)))
+        .body(Body::from(format!("priority=1&saveToFile=true&bytesizeOverride=1&fileName={}?{}", file_name.to_str().unwrap(), buf)))
         .expect("request builder");
     client.request(req).await.unwrap();
     // println!("{:#?}", client.request(req).await.unwrap().into_body());
 }
 
-async fn send_files(source_path: String, files: Vec<String>) {
+async fn send_files(source_path: PathBuf, files: Vec<PathBuf>) {
     let mut index = 0;
     let mut finished = false;
     loop {
@@ -92,8 +103,8 @@ async fn send_files(source_path: String, files: Vec<String>) {
                 finished = true;
                 break
             }
-            let source_path_copy = source_path.to_string();
-            let file_copy = files[index].to_string();
+            let source_path_copy = source_path.to_path_buf();
+            let file_copy = files[index].to_path_buf();
             handles.push(tokio::spawn(async move {
                 send_file(&source_path_copy, file_copy).await;
                 Ok(())
@@ -115,40 +126,31 @@ async fn send_message() {
     // println!("{:#?}", client.request(req).await.unwrap().into_body());
 }
 
-async fn start_server(msg_store_dir: String) -> JoinHandle<Output> {
+async fn start_server(msg_store_dir: PathBuf) -> JoinHandle<Output> {
     tokio::spawn(async move {
         Command::new("msg-store-http-server").args([
             "--node-id=12345",
             "--database=leveldb",
-            &format!("--leveldb-path={}/leveldb", msg_store_dir),
-            &format!("--file-storage-path={}/file-storage", msg_store_dir)
+            &format!("--leveldb-path={}/leveldb", msg_store_dir.to_str().unwrap()),
+            &format!("--file-storage-path={}/file-storage", msg_store_dir.to_str().unwrap())
         ]).output().unwrap()
     })
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let msg_store_dir = "msg-store-test";
-    let source_path = format!("{}/source-files", msg_store_dir);
-    let tmp_dir = TempDir::new(msg_store_dir).unwrap();
-    // let server_handle = start_server(msg_store_dir.to_string());
-    // thread::sleep(Duration::from_secs(2));
-    // // remove_dir_all(source_path).unwrap();
+    let msg_store_dir = Path::new("msg-store-test");
+    let source_path = {
+        let mut source_path = msg_store_dir.to_path_buf();
+        source_path.push("source-files");
+        source_path
+    };
     create_dir_all(&source_path).unwrap();
     println!("Creating files...");
     let file_names = create_files(&source_path, 1000);
     println!("Sending files...");
     send_files(source_path, file_names).await;
-    // for file_name in file_names.into_iter() {
-    //     send_file(&source_path, file_name).await
-    // }
-    // drop(tmp_dir);
     println!("Removing tmp dir...");
-    tmp_dir.close().unwrap();
-    // server_handle;
-    // println!("{:#?}", file_names);
-    // for _ in 0..100 {
-    //     send_file(source_path, ).await
-    // }
+    remove_dir_all(msg_store_dir).unwrap();
     Ok(())
 }
